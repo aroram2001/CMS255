1d30c704f9e3103af8fb07ad20ac63ca680f8980
aroram2001 <69920932+aroram2001@users.noreply.github.com>
2020-08-19 14:57:29 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,25 @@
+//Sublime
+//Muskaan Arora
+//August 24, 2020
+//CMS255 Section 2
+
+public class Hello {
+
+
+
+  public static void main(String[] args) {
+      
+      System.out.println("Muskaan Arora");
+      System.out.println("Lab 1");
+      System.out.println("Installing and Running Java");
+      System.out.println("August 19, 2020");
+      System.out.println("CMS255 Section 2");
+      System.out.println();
+     }
+ }
+  
+
+
+     
+
+
